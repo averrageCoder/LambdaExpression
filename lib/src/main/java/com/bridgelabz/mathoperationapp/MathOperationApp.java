7da92bdbfc1c382a8624760e9a0ab228b69025d5
Harsh R Jain <rjainharsh@gmail.com>
2021-09-13 16:38:56 +0530

    ADDED: ability to pass lambda function to static method

--- lib/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
+++ lib/src/main/java/com/bridgelabz/mathoperationapp/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.bridgelabz.mathoperationapp;
 @FunctionalInterface
 interface IMathFunction {
 	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, IMathFunction fobj) {
+		System.out.println("Result of "+function+" is "+fobj.calculate(a, b));
+	}
 }
 
 public class MathOperationApp {
@@ -18,6 +21,11 @@ public class MathOperationApp {
 		System.out.println("Subtraction is "+subtract.calculate(6, 3));
 		System.out.println("Multiplication is "+multiply.calculate(6, 3));
 		System.out.println("Division is "+divide.calculate(6, 3));
+		
+		IMathFunction.printResult(6, 3, "Addition", add);
+		IMathFunction.printResult(6, 3, "Subtraction", subtract);
+		IMathFunction.printResult(6, 3, "Multiplication", multiply);
+		IMathFunction.printResult(6, 3, "Division", divide);
 
 	}
 
