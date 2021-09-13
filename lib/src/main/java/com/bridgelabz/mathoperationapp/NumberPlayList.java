e7137e3eb823c18be79ea6467f5aa2362c2906b9
Harsh R Jain <rjainharsh@gmail.com>
2021-09-13 17:16:26 +0530

    ADDED: using proper class, anonymus class & lambda func to print elements

---
+++ lib/src/main/java/com/bridgelabz/mathoperationapp/NumberPlayList.java
@@ -0,0 +1,54 @@
+package com.bridgelabz.mathoperationapp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+
+	public static void main(String[] args) {
+		
+		List<Integer> myNumberList = new ArrayList<>();
+		for(int i=0; i<5; i++) {
+			myNumberList.add(i);
+		}
+		
+		//
+		Iterator<Integer> itr = myNumberList.iterator();
+		while(itr.hasNext()) {
+			Integer i = itr.next();
+			System.out.println("Method1: Iterator value::"+i);
+		}
+		
+		//
+		class MyConsumer implements Consumer<Integer> {
+			@Override
+			public void accept(Integer t) {
+				System.out.println("Method2: Comsumer impl value::"+t);
+			}
+		}
+		
+		MyConsumer action = new MyConsumer();
+		myNumberList.forEach(action);
+		
+		//
+		myNumberList.forEach(new Consumer<Integer>() {		
+			public void accept (Integer t) {
+				System.out.println("Method3: forEach Anonymouse class value::"+t);
+			}	
+		});
+		
+		//
+		Consumer<Integer> myListAction = n-> {
+			System.out.println("Method4: forEach Lambda impl value::"+n);
+		};
+		myNumberList.forEach(myListAction);
+		
+		//
+		myNumberList.forEach(n-> {
+			System.out.println("Method5: foreach lambda impl value::"+n);
+		});
+	}
+
+}
